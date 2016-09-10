b1b2e99125c264c0d7bf162410edd04b0974a015
Pawel <pawdro13@gmail.com>
2016-09-10 19:18:21 +0200

    Strona internetowa po testowaniu

--- StronaInternetowa/script.js
+++ StronaInternetowa/script.js
@@ -1,68 +1,49 @@
 $(document).ready(function() {
-
    $( "#bp" ).click(function() {
    $( "#contain" ).load( "projects/projects.html" );
    });
 })
-
-
 $(document).ready(function() {
-
    $( "#bm" ).click(function() {
     $( "#contain" ).load( "part.html" );
    });
 })
-
 $(document).ready(function() {
-
    $( "#ba" ).click(function() {
     $( "#contain" ).load( "aboutme/aboutme.html" );
    });
 })
-
 $(document).ready(function() {
-
    $( "#bc" ).click(function() {
     $( "#contain" ).load( "contact/contact.html" );
    });
 })
-
 $(document).ready(function() {
-
    $( "#bn" ).click(function() {
     $( "#contain" ).load( "projects/pm/pm.html"  );
    });
 })
-
 $(document).ready(function() {
-
    $( "#KiK" ).click(function() {
     $( "#contain" ).load( "projects/KiK/KiK.html"  );
    });
 })
-
 $(document).ready(function() {
-
    $( "#Snake" ).click(function() {
     $( "#contain" ).load( "projects/Snake/Snake.html"  );
    });
 })
-
 $(document).ready(function() {
-
    $( "#Paint" ).click(function() {
     $( "#contain" ).load( "projects/Paint/Paint.html"  );
    });
 })
 $(document).ready(function() {
-
    $( "#alg" ).click(function() {
     $( "#contain" ).load( "projects/Algorytmy/algorytmy.html"  );
    });
 })
-
 $(document).ready(function() {
-
    $( "#AS" ).click(function() {
     $( "#contain" ).load( "projects/AS/AnalizatorSemantyki.html"  );
    });
